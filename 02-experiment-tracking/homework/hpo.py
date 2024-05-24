5c3c32ae825c7e5f46f6ae0e79e7ba4efba11578
Rohmat S <61409906+rohmats@users.noreply.github.com>
2024-05-24 10:51:51 +0000

    May 24, 2024 at 10:51 AM UTC

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -41,7 +41,7 @@ def run_optimization(data_path: str, num_trials: int):
             rf = RandomForestRegressor(**params)
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
-            rmse = root_mean_squared_error(y_val, y_pred)
+            rmse = mean_squared_error(y_val, y_pred)
 
             mlflow.log_metric('rmse', rmse)
 
