695da35bd81cd0f4a4cb926cb26c5de668d7c519
Rohmat S <61409906+rohmats@users.noreply.github.com>
2024-05-24 12:01:27 +0000

    May 24, 2024 at 12:01 PM UTC

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -9,10 +9,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
 HPO_EXPERIMENT_NAME = "hpo"
-EXPERIMENT_NAME = "register_model"
+EXPERIMENT_NAME = "random-forest-best-models"
 RF_PARAMS = ['max_depth', 'n_estimators',
-            'min_samples_split', 'min_samples_leaf',
-            'random_state']
+             'min_samples_split', 'min_samples_leaf',
+             'random_state']
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment(EXPERIMENT_NAME)
@@ -31,14 +31,14 @@ def train_and_log_model(data_path, params):
     with mlflow.start_run():
         for param in RF_PARAMS:
             params[param] = int(params[param])
-        
+
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
 
         # Evaluate model on the validation and test sets
-        val_rmse = root_mean_squared_error(y_val, rf.predict(X_val))
+        val_rmse = root_mean_squared_error(y_val, rf.predict(X_val), squared=False)
         mlflow.log_metric("val_rmse", val_rmse)
-        test_rmse = root_mean_squared_error(y_test, rf.predict(X_test))
+        test_rmse = root_mean_squared_error(y_test, rf.predict(X_test), squared=False)
         mlflow.log_metric("test_rmse", test_rmse)
 
 
@@ -59,30 +59,23 @@ def run_register_model(data_path: str, top_n: int):
     client = MlflowClient()
 
     # Retrieve the top_n model runs and log the models
-    experiment_hpo = client.get_experiment_by_name(HPO_EXPERIMENT_NAME)
+    experiment = client.get_experiment_by_name(HPO_EXPERIMENT_NAME)
     runs = client.search_runs(
-        experiment_ids=experiment_hpo.experiment_id,
+        experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
         order_by=["metrics.rmse ASC"]
     )
-
-    print("training and logging models...")
-    for count,run in enumerate(runs):
+    for run in runs:
         train_and_log_model(data_path=data_path, params=run.data.params)
 
-        print(f"Run N = {count} | ID: {run.info.run_id} | Val RMSE: {round(run.data.metrics["rmse"],3)}")
-
     # Select the model with the lowest test RMSE
-    print("------------------------------------")
-    print(f"Among the {top_n} models with lower val rmse, the model with the lowest test rmse is:")
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     best_run = client.search_runs(experiment_ids=experiment.experiment_id,
-                                    run_view_type=ViewType.ACTIVE_ONLY,
-                                    max_results=1,
-                                    order_by=["metrics.test_rmse ASC"])[0]
-    
-    print(f"Model ID: {best_run.info.run_id} | Test RMSE: {round(best_run.data.metrics["test_rmse"],3)}")
+                                  run_view_type=ViewType.ACTIVE_ONLY,
+                                  max_results=1,
+                                  order_by=["metrics.test_rmse ASC"])[0]
+    print(f"Model ID: {best_run.info.run_id} | Test RMSE: {round(best_run.data.metrics['test_rmse'],3)}")
 
     # Register the best model
     mlflow.register_model(f"runs:/{best_run.info.run_id}/model", 'best_run_model')
