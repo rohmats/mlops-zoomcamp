d269f2a31b94c924c196bdb6f91af3f479fe56ad
Rohmat S <61409906+rohmats@users.noreply.github.com>
2024-05-24 10:46:03 +0000

    May 24, 2024 at 10:46 AM UTC

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -6,7 +6,7 @@ import numpy as np
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
@@ -34,11 +34,16 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        with mlflow.start_run():
+            mlflow.set_tag('model', 'RandomForestRegressor')
+            mlflow.log_params(params)
+            
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = root_mean_squared_error(y_val, y_pred)
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric('rmse', rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
