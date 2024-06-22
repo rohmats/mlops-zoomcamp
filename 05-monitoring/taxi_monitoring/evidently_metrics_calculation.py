dd33eeb6ecaf4ed912a12f680ee57c0257f1d8a3
Rohmat S <61409906+rohmats@users.noreply.github.com>
2024-06-22 19:55:10 +0000

    June 22, 2024 at 7:55 PM UTC

--- 05-monitoring/taxi_monitoring/evidently_metrics_calculation.py
+++ 05-monitoring/taxi_monitoring/evidently_metrics_calculation.py
@@ -68,10 +68,10 @@ report = Report(metrics = [
 @task
 def prep_db():
 	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
-		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='taxi_db'")
 		if len(res.fetchall()) == 0:
-			conn.execute("create database test;")
-		with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
+			conn.execute("create database taxi_db;")
+		with psycopg.connect("host=localhost port=5432 dbname=taxi_db user=postgres password=example") as conn:
 			conn.execute(create_table_statement)
 
 # Function to calculate metrics using Evidently
@@ -93,7 +93,7 @@ def calculate_metrics_postgresql(curr, i):
 	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
 
 	curr.execute(
-		"insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
+		"insert into taxi_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
 		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
 	)
 
@@ -103,7 +103,7 @@ def calculate_metrics_postgresql(curr, i):
 def batch_monitoring_backfill():
 	prep_db()
 	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
-	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
+	with psycopg.connect("host=localhost port=5432 dbname=taxi_db user=postgres password=example", autocommit=True) as conn:
 		for i in range(0, 27):
 			with conn.cursor() as curr:
 				calculate_metrics_postgresql(curr, i)
